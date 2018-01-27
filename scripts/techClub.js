1af656e525d2190e8be781e55e9078872b501723
Dukeboxz <u04sjj@gmail.com>
2018-01-27 20:42:21 +0000

    Completion of front page

--- scripts/techClub.js
+++ scripts/techClub.js
@@ -1,6 +1,6 @@
 
 
-  function getDateOfNextMeeting(){
+  function getDatesOfFutureMeetings(){
 
     var meetingDates = [];
     var feb = new Date("Feb 17 2018");
@@ -18,26 +18,47 @@
     meetingDates.push(feb, march, april, may, june, july, aug, sep, oct, nov);
 
     var meetingDate;
-    var todaysDate = new Date("June 5 2018");
+    var todaysDate = new Date();
 
+      var remainingMeetingDates = [];
     for(var i = 0 ; i < meetingDates.length; i++){
 
-      meetingDate = meetingDates[i];
-      if(meetingDate > todaysDate){
-        break;
+
+      if(meetingDates[i].getTime() >= todaysDate.getTime()){
+
+        var temp = meetingDates[i];
+
+      remainingMeetingDates.push(temp);
+
       }
     }
-    return meetingDate;
+    return remainingMeetingDates;
   }
 
   function displayDateOfNextMeeting(){
 
     console.log("date of next meeting");
 
-    var dateOfNextMeeting = getDateOfNextMeeting();
+    var remainingDates = getDatesOfFutureMeetings();
+    console.log("mD " + remainingDates.length);
+    var dateOfNextMeeting = remainingDates[0].toDateString();
 
     var meetingDisplay = document.getElementById('meetingDates');
+    var futureMeetingDatesDisplay = document.getElementById("futureDates");
 
-    meetingDisplay.innerHTML = dateOfNextMeeting;
+    meetingDisplay.innerHTML = "Date Of Next Meeting <br>" + dateOfNextMeeting + " 14:00";
+    var htmlFragment = "";
+    for(var i = 1 ; i < remainingDates.length; i++){
+      var nextDate = remainingDates[i].toDateString();
+      htmlFragment += nextDate + " 14:00" + "<br>";
+
+    }
+    futureMeetingDatesDisplay.innerHTML = htmlFragment;
 
   }
+
+  $(document).ready(function(){
+    $("#seeFutureDates").click(function(){
+        $("#futureDates").slideToggle("slow");
+    });
+});
